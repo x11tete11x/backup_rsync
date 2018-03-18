e79b319308176060e2e006dc7b6f80d0d618c2c5
Tete Plaza <11tete11@gmail.com>
2018-03-18 02:28:28 -0300

    Initial Files

---
+++ backup_rsync.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+################CONFIG##################
+TARGET="backup_<target>"
+DIA=`date +%d`
+MES=`date +%m`
+ANIO=`date +%Y`
+HORA=`date +%H%M%S`
+TIMESTAMP="$ANIO$MES$DIA""_""$HORA"
+HOSTNAME="$(hostname)"
+BACKUPDIR="/backup/archivo/"
+SRC_RSYNC="/<target>/"
+DST_RSYNC="/backup/$TARGET/<target>"
+########################################
+
+#Creamos directorios si no existen
+mkdir -p "$DST_RSYNC"
+mkdir -p "$BACKUPDIR"
+
+RSYNC=$(rsync -atiu $SRC_RSYNC $DST_RSYNC --delete)
+
+#PARA DEBUG
+#echo $RSYNC
+#/PARA DEBUG
+
+if [ -n "${RSYNC}" ]; then
+	#Generamos el Backup
+	FILE="$HOSTNAME""-$TARGET-""$TIMESTAMP"".tar.bz2"
+	tar -cpjf "$BACKUPDIR""$FILE" "$DST_RSYNC"
+	sha256sum "$BACKUPDIR""$FILE" > "$BACKUPDIR""$FILE"".sha256"
+else
+	echo "RSYNC no produjo cambios"
+fi
